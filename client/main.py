cceec63f28254a1f1cec402c6aaf3d950c433399
Emrys-Hong <hongpengfei.emrys@gmail.com>
2024-05-17 14:32:39 +0800

    Update

--- client/main.py
+++ client/main.py
@@ -448,7 +448,7 @@ def get_external_partitions():
     partitions = psutil.disk_partitions()
     for partition in partitions:
         usage = psutil.disk_usage(partition.mountpoint)
-        if usage.total > 1e9:
+        if usage.total > 1e10:
             directories.add(partition.mountpoint)
     return directories - set(["/"])
 
